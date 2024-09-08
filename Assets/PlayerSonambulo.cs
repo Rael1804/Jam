178ffadc805dce8ba70e1858c7ba2f98782dc164
Rael1804 <1.aznar.80.villegas.4.i@gmail.com>
2024-09-08 17:54:27 +0200

    camino sonambulo hecho

--- Assets/PlayerSonambulo.cs
+++ Assets/PlayerSonambulo.cs
@@ -7,23 +7,28 @@ public class PlayerSonambulo : MonoBehaviour
 {
     public NavMeshAgent navMesh; 
     public Transform destino;
-    // Start is called before the first frame update
+
+    public GameObject victoria;
+    public GameObject derrota;
+
     void Start()
     {
         navMesh.destination= destino.position;
     }
 
-    // Update is called once per frame
-    /*void Update()
-    {
-        navMesh.destination= destino.position;
-    }*/
-
     void OnTriggerEnter(Collider other){
         if(other.tag=="WayPoint"){
-            destino= other.gameObject.GetComponent<Waypoints>().nextPoint;
-            navMesh.destination= destino.position;   
-
+            if(other.gameObject.name == "WP (5)"){
+                if(true){//victoria)
+                    victoria.SetActive(true);
+                    }else{
+                        derrota.SetActive(true);
+                        }//derrota
+                        this.gameObject.SetActive(false);
+            }else{
+                destino= other.gameObject.GetComponent<Waypoints>().nextPoint;
+                navMesh.destination= destino.position;  
+            }
         }
     }
 }
